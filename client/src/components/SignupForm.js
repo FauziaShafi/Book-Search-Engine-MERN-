6df865a25860840cb5207bff8947584120b9801d
Fauzia Shafi <fauziashafi12@gmail.com>
2022-03-09 16:14:22 -0800

    Client: Add signup code

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from "../utils/mutations";
 
-import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
+  const [addUser] = useMutation(ADD_USER)
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
   // set state for form validation
@@ -28,25 +30,23 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
+      const { data } = await addUser({variables:userFormData });
+      const token = data.addUser.token;
       Auth.login(token);
-    } catch (err) {
-      console.error(err);
+    } catch (error) {
+      console.error(error);
       setShowAlert(true);
+      
     }
 
     setUserFormData({
-      username: '',
-      email: '',
-      password: '',
-    });
+      username : "",
+      email : "",
+      password : "",
+    })
+
+   
+      
   };
 
   return (
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require("apollo-server-express");
+const { AuthenticationError, UserInputError } = require("apollo-server-express");
 const { User } = require("../models");
 const { signToken } = require("../utils/auth");
 
@@ -17,11 +17,20 @@ const resolvers = {
 
   Mutation: {
     addUser: async (parent, { username, email, password }) => {
+     try {
       const user = await User.create({ username, email, password });
       const token = signToken(user);
 
       return { token, user };
+       
+     } catch (error) {
+
+       throw new UserInputError("Username and email is invalid");
+     }
     },
+
+
+
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
 
